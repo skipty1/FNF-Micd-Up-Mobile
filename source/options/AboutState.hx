6ca75641f56afb24082d3466071da2c6c1836e9d
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-05-26 02:52:23 +0700

    update AboutState.hx

--- source/options/AboutState.hx
+++ source/options/AboutState.hx
@@ -36,7 +36,7 @@ class AboutState extends MusicBeatState
 		logoBl.screenCenter();
 		logoBl.y = logoBl.y - 100;
 
-		text = new FlxText(0, 0, 0, "version 0.2.7.1" + "\n" + "ported by luckydog7", 64);
+		text = new FlxText(0, 0, 0, "Version 1.0" + "\n" + "PORTED BY" + "\n" + "Luckydog7" + "zacksgamerz" + "\n" + "klaviergayming" + "\n" + "peppy", 64);
 		text.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
 		text.screenCenter();
 		text.y = text.y + 150;
