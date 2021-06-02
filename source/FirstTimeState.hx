fcef9acd0aeaba97b563fd183bd8321e430a04fe
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-03 01:55:36 +0700

    update FirstTimeState.hx

--- source/FirstTimeState.hx
+++ source/FirstTimeState.hx
@@ -21,9 +21,8 @@ class FirstTimeState extends MusicBeatState
 		"WARNING:\nFNF: Mic'd Up may potentially trigger seizures for people with photosensitive epilepsy.Viewer discretion is advised.\n\n"
 		+ "FNF: Mic'd Up is a non-profit modification, aimed for entertainment purposes, and wasn't meant to be an attack on Ninjamuffin99"
 		+ " and/or any other modmakers out there. I was not aiming for replacing what Friday Night Funkin' was, won't be aiming for that and never"
-		+ " will be aiming for that. It was made for fun and from the love for the game itself. All of the comparisons between this and other mods are purely coincidental, unless stated otherwise."
-		+ " Oh and this engine is ported by zack, klavier, luckydog, and peppy. Any misusage of this port will lead to consequences.\n\n"
-		+ "Now with that out of the way, I hope you'll enjoy this FNF mod.\nFunk all the way.\nPress ENTER or A to proceed",
+		+ " will be aiming for that. It was made for fun and from the love for the game itself. All of the comparisons between this and other mods are purely coincidental, unless stated otherwise.\n"
+		+ " Oh and this engine is ported by zack, klavier, luckydog, and peppy. Any misusage of this port will lead to consequences."
 		32);
 
 	override function create()
