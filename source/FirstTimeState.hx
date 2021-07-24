a425689494b1d513f9c31122163fc0ec30d01763
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-07-24 05:37:33 -0700

    update FirstTimeState.hx

--- source/FirstTimeState.hx
+++ source/FirstTimeState.hx
@@ -34,7 +34,8 @@ class FirstTimeState extends MusicBeatState
 			+ "FNF: Mic'd Up is a non-profit modification, aimed for entertainment purposes, and wasn't meant to be an attack on Ninjamuffin99"
 			+ " and/or any other modmakers out there. I was not aiming for replacing what Friday Night Funkin' was, is and will."
 			+ " It was made for fun and from the love for the game itself. All of the comparisons between this and other mods are purely coincidental, unless stated otherwise.\n\n"
-			+ "Now with that out of the way, I hope you'll enjoy this FNF mod.\nFunk all the way.\n" 
+			+ "This port is lead by luckydog and zack, and ported by the help of peppy, gamerbros and klavier\n"
+			+ "\nNow with that out of the way, I hope you'll enjoy this FNF mod.\nFunk all the way.\n" 
 			+ #if desktop "Press ENTER to proceed" #else "Tap to proceed" #end,
 			32);
 		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
@@ -49,7 +50,7 @@ class FirstTimeState extends MusicBeatState
 		sinMod += 0.007;
 		txt.y = Math.sin(sinMod) * 60 + 100;
 		
-		#if mobile
+		#if android
 		var justTouched:Bool = false;
 
 		for (touch in FlxG.touches.list)
@@ -62,7 +63,7 @@ class FirstTimeState extends MusicBeatState
 		}
 		#end
 
-		if (FlxG.keys.justPressed.ENTER #if mobile || justTouched #end)
+		if (FlxG.keys.justPressed.ENTER #if android || justTouched #end)
 		{
 			_variables.firstTime = false;
 			leftState = true;
